fec2bdca23f3047c6899ffb41600f00215b6f497
Rusiru <Rusiruanjana5@gmail.com>
2026-07-28 01:29:34 +0530

    Clean commit without build artifacts

--- electron/preload.js
+++
@@ -1,9 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron')
-
-// Expose protected methods that allow the renderer process to use
-// the ipcRenderer without exposing the entire object
-contextBridge.exposeInMainWorld('electronAPI', {
-  getDatabasePath: () => ipcRenderer.invoke('get-database-path'),
-  getLocalDbPath: () => ipcRenderer.invoke('get-local-db-path'),
-  platform: process.platform
-})

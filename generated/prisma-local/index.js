f29010dd52a7e4b8190cd98a9eb89d132c1feb7f
Rusiru <Rusiruanjana5@gmail.com>
2026-07-28 00:54:38 +0530

    fix path issue

--- generated/prisma-local/index.js
+++ generated/prisma-local/index.js
@@ -294,7 +294,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": null,
+    "rootEnvPath": "../../.env",
     "schemaEnvPath": "../../.env"
   },
   "relativePath": "../../prisma",
